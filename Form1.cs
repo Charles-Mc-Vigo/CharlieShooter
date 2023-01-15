0b1659e9358996e6ad8a9cf90af86341b3321b55
Charles Vigo <cvgithub204@gmail.com>
2023-01-15 13:53:38 +0800

    just practicing pushing on github

---
+++ Form1.cs
@@ -0,0 +1,32 @@
+using System.Media;
+
+namespace FinalProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //play background music while in form1
+            SoundPlayer bgMusic = new SoundPlayer(@"C:\Users\Charles Mc\Desktop\Final Project in Programming 2\FinalProject\bin\Debug\net6.0-windows\mixkit-bootleg-1055.wav");
+            bgMusic.PlayLooping();
+        }
+        //
+        //play the game if the play button get clicked
+        private void PlayButton_Click(object sender, EventArgs e)
+        {
+            //these are the Condition if play button get clicked
+            //
+            Form3 form3 = new Form3(); // creating instance of form3 to be able to get to that form
+            form3.ShowDialog(); // calling out the instance of form 3 to be able to get to that form
+            this.Close();
+        }
+        //
+        // program will teminate if exit button get clicked
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+    }
+}
